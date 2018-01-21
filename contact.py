8d37b3ede0bf06d860a88014f9f8eb294ec6a183
Oksana <prokopekoksana@gmail.com>
2018-01-21 14:54:55 +0100

    Zmiany_3 do scenariusza2

---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contacts:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, mobile, home, work, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.home = home
+        self.work = work
+        self.email = email
--- test_add_contact_2.py
+++ test_add_contact_2.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+
+import unittest
+from contact import Contacts
 
 def is_alert_present(wd):
     try:
@@ -14,54 +15,58 @@ class test_add_contact_2(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
-    
-    def test_test_add_contact_2(self):
-        success = True
-        wd = self.wd
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/edit.php")
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
         wd.find_element_by_name("theform").click()
-        wd.find_element_by_link_text("add new").click()
+
+    def open_add_new(self):
+        pass
+
+    def fill_the_form(self, wd, contact):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Oksana")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Oksana")
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Prokopek")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("ProOksana")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Test_title")
+        wd.find_element_by_name("title").send_keys(contact.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Test_Company")
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Wrocław, ul. Damrota 48/5")
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("500300488")
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("500300488")
+        wd.find_element_by_name("home").send_keys(contact.home)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("500300488")
+        wd.find_element_by_name("work").send_keys(contact.work)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("prokopekoksana@gmail.com")
+        wd.find_element_by_name("email").send_keys(contact.email)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[22]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[22]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[7]").is_selected():
@@ -69,10 +74,39 @@ class test_add_contact_2(unittest.TestCase):
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1991")
+
+    def submit_add_new(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    def test_add_contact_2(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_new()
+        wd.find_element_by_link_text("add new").click()
+        self.fill_the_form(wd, Contacts(firstname="Oksana", middlename="Oksana", lastname="Prokopek", nickname="ProOksana", title="Test_title", company="Test_Company",
+                           address="Wrocław, ul. Damrota 48/5", mobile="500300488", home="500300488", work="500300488",
+                           email="prokopekoksana@gmail.com"))
+        self.submit_add_new(wd)
+        self.logout(wd)
+
+    def test_add_empty_contact_2(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_new()
+        wd.find_element_by_link_text("add new").click()
+        self.fill_the_form(wd, Contacts(firstname="", middlename="", lastname="", nickname="",
+                            title="", company="",
+                            address="", mobile="", home="",
+                            work="",
+                            email=""))
+        self.submit_add_new(wd)
+        self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
