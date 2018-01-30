3727e7b5de17b1c88bb002124297c99b086fc4d3
Oksana <prokopekoksana@gmail.com>
2018-01-30 08:11:10 +0100

    Wyodrębnione 2 klasy pomocnicze dla scenariusza2

--- test/test_add_contact_2.py
+++ test/test_add_contact_2.py
@@ -12,22 +12,22 @@ def app(request):
 
 
 def test_add_contact_2(app):
-    app.login(username="admin", password="secret")
-    app.fill_the_form(Contacts(firstname="Oksana", middlename="Oksana", lastname="Prokopek", nickname="ProOksana", title="Test_title", company="Test_Company",
+    app.session2.login(username="admin", password="secret")
+    app.contact.fill_the_form(Contacts(firstname="Oksana", middlename="Oksana", lastname="Prokopek", nickname="ProOksana", title="Test_title", company="Test_Company",
                         address="Wrocław, ul. Damrota 48/5", mobile="500300488", home="500300488", work="500300488",
                         email="prokopekoksana@gmail.com"))
-    app.submit_add_new()
-    app.logout()
+    app.contact.submit_add_new()
+    app.session2.logout()
 
 def test_add_empty_contact_2(app):
-    app.login(username="admin", password="secret")
-    app.fill_the_form(Contacts(firstname="", middlename="", lastname="", nickname="",
+    app.session2.login(username="admin", password="secret")
+    app.contact.fill_the_form(Contacts(firstname="", middlename="", lastname="", nickname="",
                         title="", company="",
                         address="", mobile="", home="",
                         work="",
                         email=""))
-    app.submit_add_new()
-    app.logout()
+    app.contact.submit_add_new()
+    app.session2.logout()
 
 
 
