1a59716e337200f5249493eb5ee124b2f1ffb4ba
Oksana <prokopekoksana@gmail.com>
2018-02-03 18:34:40 +0100

    Dodany test modyfikacji kontaktów

--- fixture/contact.py
+++ fixture/contact.py
@@ -57,6 +57,28 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[7]").click()
 
+    def edit_contact(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        #editing
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("Oksana1_test")
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("Prokopek_test")
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys("OK_test")
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("Different address")
+        #submit editing
+        wd.find_element_by_name("update").click()
+
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -66,6 +88,8 @@ class ContactHelper:
         #submit deletion
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
+
+
 
 
 
