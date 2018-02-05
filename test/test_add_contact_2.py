e2973450db41a6ebd2a9c8aa116995d0942cd82b
Oksana <prokopekoksana@gmail.com>
2018-02-05 23:27:53 +0100

    Zwiększenie inteligencji fikstury: walidacja fikstury

--- test/test_add_contact_2.py
+++ test/test_add_contact_2.py
@@ -4,20 +4,18 @@ from model.contact import Contacts
 
 
 def test_add_contact_2(app):
-    app.session.login(username="admin", password="secret")
     app.contact.fill_the_form(Contacts(firstname="Oksana", middlename="Oksana", lastname="Prokopek", nickname="ProOksana", title="Test_title", company="Test_Company",
                         address="Wrocław, ul. Damrota 48/5", mobile="500300488", home="500300488", work="500300488",
                         email="prokopekoksana@gmail.com"))
-    app.session.logout()
+
 
 def test_add_empty_contact_2(app):
-    app.session.login(username="admin", password="secret")
     app.contact.fill_the_form(Contacts(firstname="", middlename="", lastname="", nickname="",
                         title="", company="",
                         address="", mobile="", home="",
                         work="",
                         email=""))
-    app.session.logout()
+
 
 
 
