c7d988543574dc536e9d61dc8a88921cb71621b3
Oksana <prokopekoksana@gmail.com>
2018-02-14 15:38:17 +0100

    Sprawdzanie równości obiektów i list
    Sprawdzanie zrobiono dla plików: usuwania grup i usuwania kontaktów.

--- model/contact.py
+++ model/contact.py
@@ -13,3 +13,9 @@ class Contacts:
         self.work = work
         self.email = email
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.firstname, self.middlename, self.lastname, self.nickname, self.title, self.company, self.address, self.mobile, self.home, self.work, self.email)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname and self.lastname == other.lastname and self.email == other.email
